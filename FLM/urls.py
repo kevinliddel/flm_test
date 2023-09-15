2d7242a8943a8501951e65668e71cc4218a99655
Kevin Liddel <ezechielevangelion@gmail.com>
2023-09-15 13:31:24 +0300

    Update urls.py for vercel

--- FLM/urls.py
+++ FLM/urls.py
@@ -16,6 +16,5 @@ urlpatterns = [
     path('users/', include('accounts.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

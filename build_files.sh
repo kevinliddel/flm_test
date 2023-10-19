316386e9f50e81bd37fef80c75d080fbdc122d26
Kevin Liddel <ezechielevangelion@gmail.com>
2023-10-19 08:24:50 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,12 @@
+# Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Apply database migrations
 python3.9 manage.py migrate
-python3.9 manage.py collectstatic
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput
+
+# Create a superuser
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='flm').exists() or User.objects.create_superuser('flm', 'admin@example.com', 'fiangonana')" | python3.9 manage.py shell
